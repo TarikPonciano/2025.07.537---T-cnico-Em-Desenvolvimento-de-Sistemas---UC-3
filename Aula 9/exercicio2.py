53e6b123fbc12ef119dea421ab03dbe8f35ea8ba
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-06-26 21:21:10 -0300

    Add number sequence and tabuada exercises
    Updated exercicio2.py to build and print a string of numbers from 0 to 200 separated by ' - ', and print the total count of even numbers. Added exercicio3.py to generate the multiplication table (tabuada) for a user-input number.

--- Aula 9/exercicio2.py
+++ Aula 9/exercicio2.py
@@ -7,10 +7,17 @@
 # else:
 #     print("É impar!")
 contador = 0
+numeros = ""
 
 for i in range(201):
-    print(i)
+    # numeros += str(i) + " - " if i != 200 else str(i)
+    if i == 200:
+        numeros += str(i)
+    else:
+        numeros +=  str(i) + " - "
     if(i%2 == 0):
         contador += 1
+
+print("Numeros:", numeros)
 
 print("Total de pares:", contador)
